c784fe558568efaea3cbaabcbad9ecf318a2a874
sliitguy <it23259416@my.sliit.lk>
2025-08-01 03:56:33 +0530

    Updated corrections

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const app = require('./src/app')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+
+const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL
+
+
+const server = http.createServer(app);
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection is ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting with MongoDB: ', err)
+})
+
+async function startServer() {
+    await mongoose.connect(MONGO_URL)
+
+    server.listen(PORT, () => {
+        console.log(`Listenining on port ${PORT}...`)
+    })
+}
+
+startServer()
